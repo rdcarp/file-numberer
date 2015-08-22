9b6ea3ed0fa50ce92622cf6b42f0a5f60ea5d0a3
Richard Carp <mail@rdcarp.com>
2015-08-22 23:53:03 +0100

    Include inadvertantly previously ignored files.

---
+++ FileNumberer.Lib/FileNamers/DefaultFileNamer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileNumberer.Lib.FileNamers
+{
+    class DefaultFileNamer : IFileNamer
+    {
+        public string GetName(int index)
+        {
+            return index.ToString("D4");
+        }
+    }
+}
